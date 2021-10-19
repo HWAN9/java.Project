f51b083eaaaa91f25557616e1e5afc593a3ed717
peaks26 <88639900+peaks26@users.noreply.github.com>
2021-10-19 16:46:49 +0900

    초기파일

---
+++ usedCar/src/controller/menuClass.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class menuClass {
+	public static void menu() {
+	System.out.println("# 중고차는 호카");
+	System.out.println("1. 내차사기");
+	System.out.println("2. 내차팔기");
+	System.out.println("3. 관리자로그인");
+	System.out.println("4. 프로그램 종료");
+	}
+}
